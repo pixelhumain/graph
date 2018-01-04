63c88471492ca474db4da08370ad6b9ec3969b9b
Oceatoon <oceatoon@gmail.com>
2018-01-04 16:34:58 +0400

    creating graph module

---
+++ controllers/CoController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * CoController.php
+ *
+ * Cocontroller always works with the PH base 
+ *
+ * @author: Tibor Katelbach <tibor@pixelhumain.com>
+ * Date: 14/03/2014
+ */
+class CoController extends Controller {
+
+
+    protected function beforeAction($action) {
+        //parent::initPage();
+		return parent::beforeAction($action);
+  	}
+
+  	public function actions()
+	{
+	    return array(
+	        'getdata'  => 'citizenToolKit.controllers.graph.GetDataAction',
+	        'viewer'   => 'citizenToolKit.controllers.graph.ViewerAction',
+	        'd3'  	   => 'citizenToolKit.controllers.graph.D3Action',
+	        'search'   => 'citizenToolKit.controllers.graph.SearchAction'
+	    );
+	}
+}
